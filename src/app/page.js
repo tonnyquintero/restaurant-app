e0a32455b8f2540fd3abac870674302ca3e101d3
tonny187 <tonyoanthony@gmail.com>
2024-05-31 19:23:18 -0500

    adding some images

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,15 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hola Mundo</h1>
+      <div className={styles.box}>
+        <div className={styles.item}><Image className={styles.img} src="/img4.png" alt="food" width={250} height={300} /></div>
+        <div className={styles.item}><Image className={styles.img} src="/img7.png" alt="food" width={250} height={300} /></div>
+        <div className={styles.item}><Image className={styles.img} src="/img3.png" alt="food" width={250} height={300} /></div>
+        <div className={styles.item}><Image className={styles.img} src="/img2.png" alt="food" width={250} height={300} /></div>
+        <div className={styles.item}><Image className={styles.img} src="/img5.png" alt="food" width={250} height={300} /></div>
+        <div className={styles.item}><Image className={styles.img} src="/img6.png" alt="food" width={250} height={300} /></div>
+        <div className={styles.item}><Image className={styles.img} src="/img7.png" alt="food" width={250} height={300} /></div>
+      </div>
     </main>
   );
 }
